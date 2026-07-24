fcc0e4ae132f2fdb4b616162a5fb968ff0cb9ed4
radhikaradhika25 <radhikatradhikat2004radhikat@gmail.com>
2026-07-24 19:09:03 +0530

    Update db_test.py

--- db_test.py
+++ db_test.py
@@ -1,13 +1,26 @@
 import psycopg2
 
-conn = psycopg2.connect(
-    host="localhost",
-    database="studyplanner",
-    user="postgres",
-    password="postgres123",
-    port="5432"
-)
+def get_connection():
+    try:
+        conn = psycopg2.connect(
+            host="localhost",
+            database="studyplanner",   
+            user="postgres",           
+            password="postgres123",    
+            port="5432"
+        )
+        return conn
 
-print("Connected successfully!")
+    except Exception as e:
+        print("Database Connection Error:", e)
+        return None
 
-conn.close()
+
+if __name__ == "__main__":
+    conn = get_connection()
+
+    if conn:
+        print("✅ Connected successfully to ShaktiDB!")
+        conn.close()
+    else:
+        print("❌ Failed to connect to ShaktiDB.")
